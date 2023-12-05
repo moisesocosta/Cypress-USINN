d1c7327b5c532be8b671027e2debad44463019fd
Moisés Oliveira Costa <mosolicos@gmail.com>
2023-12-05 10:27:39 -0300

    Atualizando a parte falha do CT 11
    Colocando a parte de falha na conexão

--- cypress/e2e/CT-US-011.cy.js
+++ cypress/e2e/CT-US-011.cy.js
@@ -13,4 +13,27 @@ describe('CT-US-011 | Listar meus diagramas', function(){
     //Acessa a página de "Documentos"
     cy.documentos_teste()
   })
+
+  it.only('FALHA(Falha na conexão) - Listar meus diagramas', () => {
+    //Faz o login
+    cy.login_teste(credentials.email, credentials.password)
+
+    cy.intercept({
+      method: 'GET',
+      url: 'https://usinnmodeler.vercel.app/dashboard',
+    }, (req) => {
+      console.log('Interceptando a solicitação...');
+      req.continue((res) => {
+        res.statusCode = 500;
+      });
+    }).as('failRequest');
+
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
+
+    cy.wait('@failRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(500);
+    });
+
+  })
 });
