433895be1ae91942c073df8846e77b20b3056f7d
Moisés Oliveira Costa <mosolicos@gmail.com>
2024-05-07 16:07:07 -0300

    adição de um novo método para testar

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 880,
+  viewportWidth: 1280,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
     chromeWebSecurity: false,
     video: true,
+    experimentalWebKitSupport: true,
     projectId: "p9t27r"
   },
 });
--- cypress/e2e/produto-final/funcional/CT-US-002.cy.js
+++ cypress/e2e/produto-final/funcional/CT-US-002.cy.js
@@ -2,55 +2,81 @@ const credentials = require('../../../fixtures/credentials.json')
 const credentialsWrong = require('../../../fixtures/credentialsWrong.json')
 
 describe('CT-US-002 | Acesso ao sistema', function(){
-  beforeEach(() => {
-    //Acessa a página de "Login"
-    cy.visit('https://usinnmodeler.vercel.app/login')
-  })
+  context('Dado que estou na página de login da USINN Modeler', () => {
+    beforeEach(() => {
+      //Acessa a página de "Login"
+      cy.visit('https://usinnmodeler.vercel.app/login');
+    })
 
-  it('SUCESSO - Login realizado com sucesso', () => {
-    //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
+    context('Quando preencho os campos com dados válidos', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentials.email, credentials.password)
+      })
+      
+      it('Então recebo uma mensagem de sucesso, o acesso ao sistema é liberado e sou redirecionado para a página inicial.', () => {
+          cy.get('[id="dashboard"]').should('exist')
+      })
+    })
 
-    cy.get('[id="dashboard"]').should('exist')
-  })
+    context('Quando preencho os campos com dados inválidos(Email diferente)', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentialsWrong.wrongEmail, credentials.password)
+      })
+      
+      it('Então recebo uma mensagem de erro e indicação de que os campos precisam ser preenchidos corretamente.', () => {
+        cy.get('[id="dashboard"]').should('not.exist')
+        cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
+      })
+    })
 
-  it('FALHA(Email diferente) - Inserção de dados inválidos', () => {
-    //Faz o login
-    cy.login_teste(credentialsWrong.wrongEmail, credentials.password)
+    context('Quando preencho os campos com dados inválidos(Senha diferente)', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentials.email, credentialsWrong.wrongPassword)
+      })
+      
+      it('Então recebo uma mensagem de erro e indicação de que os campos precisam ser preenchidos corretamente.', () => {
+        cy.get('[id="dashboard"]').should('not.exist')
+        cy.get('.swal2-popup').should('contain', 'Credenciais inválidas')
+      })
+    })
 
-    cy.get('[id="dashboard"]').should('not.exist')
-    cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
-  })
+    context('Quando preencho os campos com dados inválidos(Email e senha diferentes)', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentialsWrong.wrongEmail, credentialsWrong.wrongPassword)
+      })
+      
+      it('Então recebo uma mensagem de erro e indicação de que os campos precisam ser preenchidos corretamente.', () => {
+        cy.get('[id="dashboard"]').should('not.exist')
+        cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
+      })
+    })
 
-  it('FALHA(Senha diferente) - Inserção de dados inválidos', () => {
-    //Faz o login
-    cy.login_teste(credentials.email, credentialsWrong.wrongPassword)
+    context('Quando preencho os campos com dados inválidos(Domínio diferente)', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentialsWrong.wrongEmailDomain, credentials.password)
+      })
+      
+      it('Então recebo uma mensagem de erro e indicação de que os campos precisam ser preenchidos corretamente.', () => {
+        cy.get('[id="dashboard"]').should('not.exist')
+        cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
+      })
+    })
 
-    cy.get('[id="dashboard"]').should('not.exist')
-    cy.get('.swal2-popup').should('contain', 'Credenciais inválidas')
-  })
-
-  it('FALHA(Email e senha diferentes) - Inserção de dados inválidos', () => {
-    //Faz o login
-    cy.login_teste(credentialsWrong.wrongEmail, credentialsWrong.wrongPassword)
-
-    cy.get('[id="dashboard"]').should('not.exist')
-    cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
-  })
-
-  it('FALHA(Domínio diferente) - Falha ao realizar login', () => {
-    //Faz o login
-    cy.login_teste(credentialsWrong.wrongEmailDomain, credentials.password)
-
-    cy.get('[id="dashboard"]').should('not.exist')
-    cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
-  })
-
-  it('FALHA(Domínio e senha diferentes) - Falha ao realizar login', () => {
-    //Faz o login
-    cy.login_teste(credentialsWrong.wrongEmailDomain, credentialsWrong.wrongPassword)
-    
-    cy.get('[id="dashboard"]').should('not.exist')
-    cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
+    context('Quando preencho os campos com dados inválidos(Domínio e senha diferentes)', () => {
+      beforeEach(() => {
+        //Faz o login
+        cy.login_teste(credentialsWrong.wrongEmailDomain, credentialsWrong.wrongPassword)
+      })
+      
+      it('Então recebo uma mensagem de erro e indicação de que os campos precisam ser preenchidos corretamente.', () => {
+        cy.get('[id="dashboard"]').should('not.exist')
+        cy.get('.swal2-popup').should('contain', 'Usuário não encontrado')
+      })
+    })
   })
 });
