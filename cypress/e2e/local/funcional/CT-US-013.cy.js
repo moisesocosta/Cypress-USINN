15f4c86c91f723db3f965cefd629326d5d5eadd9
Moisés Oliveira Costa <mosolicos@gmail.com>
2024-06-13 14:04:45 -0300

    corrigindo a us013

--- cypress/e2e/local/funcional/CT-US-013.cy.js
+++ cypress/e2e/local/funcional/CT-US-013.cy.js
@@ -4,97 +4,80 @@ describe('CT-US-013 | Baixar diagrama', function(){
     cy.visit('http://localhost:3000/login')
   })
 
-  it.only('SUCESSO(Formato PNG) - Exportar diagrama com sucesso', () => {
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato PNG)', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Escolhe a opção de PNG
-    cy.get('.form-select').select(1)
+    cy.get('.input-group > .form-select').select(1)
     cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  it('SUCESSO(Formato JPEG) - Exportar diagrama com sucesso', () => {
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato JPEG)', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Escolhe a opção de JPEG
-    cy.get('.form-select').select(2)
+    cy.get('.input-group > .form-select').select(2)
     cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  it('SUCESSO(Formato WEBP) - Exportar diagrama com sucesso', () => {
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato WEBP)', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Escolhe a opção de WEBP
-    cy.get('.form-select').select(3)
+    cy.get('.input-group > .form-select').select(3)
     cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  it('SUCESSO(Formato SVG) - Exportar diagrama com sucesso', () => {
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato SVG)', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Escolhe a opção de SVG
-    cy.get('.form-select').select(4)
+    cy.get('.input-group > .form-select').select(4)
     cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  it('SUCESSO(Formato PDF) - Exportar diagrama com sucesso', () => {
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato PDF)', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Escolhe a opção de PDF
-    cy.get('.form-select').select(5)
+    cy.get('.input-group > .form-select').select(5)
     cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  it('FALHA - Cancelar - Exportar Diagrama ', () => {
+  it('Cenário 02: Cancelar - Exportar Diagrama ', () => {
     //Faz o login
-    cy.login_teste(credentials.email, credentials.password)
-
-    //Acessa a página de Documentos
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
     cy.documentos_teste()
-
+  
     //Clica no botão de "Exportar"
     cy.exportarDiagramas_teste()
-
     //Clica em cancelar
     cy.get('#exportModalId > .modal-dialog > .modal-content > .modal-header > #closeModal').click()
-  })
-
-  it('FALHA - Falha de conexão ao exportar diagrama', () => {
-    
-
   })
 });
--- cypress/e2e/produto-final/funcional/CT-US-013.cy.js
+++ cypress/e2e/produto-final/funcional/CT-US-013.cy.js
@@ -1,187 +1,83 @@
 describe('CT-US-013 | Exportar diagrama', function(){
-  describe('Cenário 01: Exportar Diagrama com sucesso(Formato PNG)', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
-  
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
-
-        context('E clico no botão exportar', () => {
-          beforeEach(() => {
-            //Escolhe a opção de PNG
-            cy.get('.input-group > .form-select').select(1)
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
-          })
-        })
-      })
-    })
+  beforeEach(() => {
+    //Acessa a página de "Cadastro"
+    cy.visit('https://usinnmodeler.vercel.app/login')
   })
 
-  describe('Cenário 01: Exportar Diagrama com sucesso(Formato JPEG)', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato PNG)', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
   
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
-
-        context('E clico no botão exportar', () => {
-          beforeEach(() => {
-            //Escolhe a opção de PNG
-            cy.get('.input-group > .form-select').select(2)
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
-          })
-        })
-      })
-    })
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Escolhe a opção de PNG
+    cy.get('.input-group > .form-select').select(1)
+    cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  describe('Cenário 01: Exportar Diagrama com sucesso(Formato WEBP)', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato JPEG)', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
   
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
-
-        context('E clico no botão exportar', () => {
-          beforeEach(() => {
-            //Escolhe a opção de PNG
-            cy.get('.input-group > .form-select').select(3)
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
-          })
-        })
-      })
-    })
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Escolhe a opção de JPEG
+    cy.get('.input-group > .form-select').select(2)
+    cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  describe('Cenário 01: Exportar Diagrama com sucesso(Formato SVG)', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato WEBP)', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
   
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
-
-        context('E clico no botão exportar', () => {
-          beforeEach(() => {
-            //Escolhe a opção de PNG
-            cy.get('.input-group > .form-select').select(4)
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
-          })
-        })
-      })
-    })
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Escolhe a opção de WEBP
+    cy.get('.input-group > .form-select').select(3)
+    cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  describe('Cenário 01: Exportar Diagrama com sucesso(Formato PDF)', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato SVG)', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
   
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
-
-        context('E clico no botão exportar', () => {
-          beforeEach(() => {
-            //Escolhe a opção de PNG
-            cy.get('.input-group > .form-select').select(5)
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
-          })
-        })
-      })
-    })
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Escolhe a opção de SVG
+    cy.get('.input-group > .form-select').select(4)
+    cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
   })
 
-  describe('Cenário 02: Cancelar - Exportar Diagrama ', () => {
-    context('Dado que na página de Edição de Diagrama', () => {
-      beforeEach(() => {
-        //Acessa a página de "Cadastro"
-        cy.visit('https://usinnmodeler.vercel.app/login')
-        //Faz o login
-        cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
-        //Acessa a página de "Documentos"
-        cy.documentos_teste()
-      })
+  it('Cenário 01: Exportar Diagrama com sucesso(Formato PDF)', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
   
-      context('Quando clico não exportar como e escolho o formato desejado', () => {
-        beforeEach(() => {
-          //Clica no botão de "Exportar"
-          cy.exportarDiagramas_teste()
-        })
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Escolhe a opção de PDF
+    cy.get('.input-group > .form-select').select(5)
+    cy.get('.swal2-popup').should('contain', 'Diagrama exportado com sucesso!')
+  })
 
-        context('E clico em cancelar', () => {
-          beforeEach(() => {
-            //Clica em cancelar
-            cy.get('#exportModalId > .modal-dialog > .modal-content > .modal-header > #closeModal').click()
-          })
-
-          it('Então visualizo a página dos diagramas', () => {
-            
-          })
-        })
-      })
-    })
+  it('Cenário 02: Cancelar - Exportar Diagrama ', () => {
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+    //Acessa a página de "Documentos"
+    cy.documentos_teste()
+  
+    //Clica no botão de "Exportar"
+    cy.exportarDiagramas_teste()
+    //Clica em cancelar
+    cy.get('#exportModalId > .modal-dialog > .modal-content > .modal-header > #closeModal').click()
   })
 });
