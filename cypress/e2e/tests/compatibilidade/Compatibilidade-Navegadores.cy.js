65eac7d6263827be80f5da0d9d2a8e68e69e9ce2
Moisés Oliveira Costa <mosolicos@gmail.com>
2024-06-26 15:58:14 -0300

    Update Compatibilidade

--- cypress/e2e/tests/compatibilidade/Compatibilidade-Navegadores.cy.js
+++ cypress/e2e/tests/compatibilidade/Compatibilidade-Navegadores.cy.js
@@ -35,4 +35,15 @@ describe('Visualizar compatibilidade da USINN Modeler com o navegador Google Chr
 
     cy.get('[id="dashboard"]').should('exist')
   })
+
+  it.only('Apenas executado quando é no Safari', {browser: 'webkit'},() => {
+    cy.get('#APjFqb').type('usinn modeler{enter}')
+    cy.get('#rso > :nth-child(1) > .g > .N54PNb > .jGGQ5e > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
+    cy.get('.d-flex > .bg-white').click()
+
+    //Faz o login
+    cy.login_teste(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'))
+
+    cy.get('[id="dashboard"]').should('exist')
+  })
 });
