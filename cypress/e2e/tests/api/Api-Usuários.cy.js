c46e5a61034c5b51a8f99e73d63bd2870cb70b0e
Pedro B. <pedrobarreto@alu.ufc.br>
2024-06-27 16:08:14 -0300

    fix: signin e signup da api

--- cypress/e2e/tests/api/Api-Usuários.cy.js
+++ cypress/e2e/tests/api/Api-Usuários.cy.js
@@ -1,45 +1,53 @@
+import { faker } from '@faker-js/faker';
+const email = faker.internet.email()
+
 describe('Realizar requisição de Login e Cadastro via API', function(){
-  it('Realizar requisição de Login', () => {
-    cy.api({
-      method: 'POST',
-      url: 'http://localhost:8080',
-      body: {
-        name: Cypress.env('USER_API'),
-        email: Cypress.env('USER_API_EMAIL'),
-        password: Cypress.env('USER_API_PASSWORD')
-      },
-      failOnStatusCode: false
-    }).then((response) => {
-      expect(response.status).to.equal(404)
-    })
-  })
+  let TokenResposta;
 
   it('Realizar requisição de Cadastro', () => {
     cy.api({
       method: 'POST',
-      url: 'http://localhost:8080',
+      url: 'http://localhost:8080/api/signup',
       body: {
-        name: Cypress.env('USER_API'),
-        email: Cypress.env('USER_API_EMAIL'),
-        password: Cypress.env('USER_API_PASSWORD'),
-        company: Cypress.env('USER_API_COMPANY'),
-        office: Cypress.env('USER_API_OFFICE')
+        name: Cypress.env('USER_NAME'),
+        email: email,
+        password: Cypress.env('USER_PASSWORD'),
+        company: Cypress.env('USER_COMPANY'),
+        birthday: '2014-06-27',
+        gender: Cypress.env('USER_GENDER'),
+        role: Cypress.env('USER_ROLE'),
+        accept: true
       },
       failOnStatusCode: false
     }).then((response) => {
-      expect(response.status).to.equal(404)
+      expect(response.status).to.equal(200)
+    })
+  })
+
+  it('Realizar requisição de Login', () => {
+    cy.api({
+      method: 'POST',
+      url: 'http://localhost:8080/api/signin',
+      body: {
+        email: email,
+        password: Cypress.env('USER_PASSWORD')
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.equal(200)
       expect(response.body.token).to.exist
-      let TokenResposta = response.body.token
+      TokenResposta = response.body.token;
     })
   })
 
   it('Listar usuários estando logado', () => {
     cy.api({
       method: 'GET',
-      url: 'http://localhost:8080',
+      url: 'http://localhost:8080/api/user',
       headers: {
         Authorization: TokenResposta
       },
-    }).then(response => response.body[0]).should('have.keys', ['name', 'birthday', 'role', 'email', 'password', 'passwordLocal', 'gender', 'company'])
+      failOnStatusCode: false
+    }).then(response => response.body[0]).should('have.keys', ['name', 'birthday', 'role', 'email', 'password', 'gender', 'company'])
   })
 });
